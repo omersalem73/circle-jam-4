515f00fd13f54068e28b03157b3386867a33dadd
Idan <idanruf1@gmail.com>
2020-12-01 16:16:31 +1100

    update exit points ui

--- questions_stages.py
+++ questions_stages.py
@@ -4,6 +4,7 @@ import arcade
 
 from globals import get_game, sleep_before, add_timer, SCREEN_WIDTH, SCREEN_HEIGHT
 from label import Label
+from ui_base import VisibilityToggle
 
 
 class StageType(Enum):
@@ -23,19 +24,20 @@ STAGES = [Stage(100), Stage(200), Stage(500), Stage(1000, StageType.EXIT_POINT),
           Stage(500000), Stage(1000000, StageType.EXIT_POINT)]
 
 
-class QuestionsStages:
+class QuestionsStages(VisibilityToggle):
 
     def __init__(self):
+        super().__init__()
         self._current_stage_index = 0
         self._stages = []
         for stage in STAGES:
-            color = arcade.color.BLACK if stage.stage_type is StageType.NORMAL else arcade.color.WHITE
+            color = arcade.color.ORANGE if stage.stage_type is StageType.NORMAL else arcade.color.WHITE
             self._stages.append(Label('${}'.format(stage.money), 0, 0, color))
 
         self._horizon_padding = 30
         self._max_width = max([lbl.get_size()[0] for lbl in self._stages])
         height_sum = sum([lbl.get_size()[1] for lbl in self._stages]) + (10 * (len(self._stages) - 1))
-        current_y = 165
+        current_y = 100
         for i, lbl in enumerate(self._stages):
             _, h = lbl.get_size()
             lbl.x = SCREEN_WIDTH - self._max_width - self._horizon_padding
@@ -48,7 +50,8 @@ class QuestionsStages:
         self._current_stage_index = 0
         get_game().on_screen_question.reset_data()
         get_game().audience_share.update()
-        add_timer(0.5, lambda: get_game().question_pool.show())
+        add_timer(0.5, get_game().question_pool.show)
+        self.show()
 
     @sleep_before(2)
     def next_stage(self):
@@ -60,10 +63,15 @@ class QuestionsStages:
             prev_question = get_game().on_screen_question.question_data
             get_game().on_screen_question.reset_data()
             get_game().audience_share.update(prev_question)
-            add_timer(0.5, lambda: get_game().question_pool.show())
+            add_timer(0.5, get_game().question_pool.show)
+            self.show()
 
-    def on_draw(self):
+    def draw_if_visible(self):
+        if self._current_stage_index > 0:
+            previous_lbl = self._stages[self._current_stage_index - 1]
+            previous_lbl.color = arcade.color.ORANGE
         selected_lbl = self._stages[self._current_stage_index]
+        selected_lbl.color = arcade.color.BLACK
         w, h = selected_lbl.get_size()
         arcade.draw_rectangle_filled(selected_lbl.x + self._max_width / 2, selected_lbl.y + h / 2,
                                      self._max_width + self._horizon_padding, h + self._horizon_padding / 4,
